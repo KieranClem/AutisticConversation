94e2625b7fe14b228aa72c489a99238aa0c7cf07
KieranClem <76912798+KieranClem@users.noreply.github.com>
2023-12-09 19:58:13 +0000

    final test build changes

--- Assets/Scripts/TextBoxActions.cs
+++ Assets/Scripts/TextBoxActions.cs
@@ -6,25 +6,30 @@ public class TextBoxActions : MonoBehaviour
 {
     //This note is more an idea for later: maybe make it so when stress gets worse the text boxes start to shake, making it harder to ignore?
 
-
+    private float timeOnScene;
     public float timeTextboxIsActive;
     
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(EndExistence());
+        //StartCoroutine(EndExistence());
+        timeOnScene = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeOnScene += Time.deltaTime;
+        if(timeOnScene > timeTextboxIsActive)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
-    IEnumerator EndExistence()
+    /*IEnumerator EndExistence()
     {
         yield return new WaitForSeconds(timeTextboxIsActive);
         GameObject.FindGameObjectWithTag("StressManager").GetComponent<StressMeter>().textBoxesThatHaveBeenSpawned.Remove(this.gameObject);
         Destroy(this.gameObject);
-    }
+    }*/
 }
